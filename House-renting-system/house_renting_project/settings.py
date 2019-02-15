9a9bb593398c09072f7b2b82ccbcc61fbbbde82a
ModishSanchit <ModishSanchit@gmail.com>
2019-02-16 03:02:41 +0530

    password validation module is included

--- House-renting-system/house_renting_project/settings.py
+++ House-renting-system/house_renting_project/settings.py
@@ -120,25 +120,25 @@ DATABASES = {
     }
 }
 
-#
-## Password validation
+
+##Password validation
 ## https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 #
-#AUTH_PASSWORD_VALIDATORS = [
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#    },
-#]
-#
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+                         ]
+
 #PASSWORD_HASHERS=[
 #    'django.contrib.auth.hashers.Argon2PasswordHasher',
 #   # 'django.contrib.auth.hashers.BCrypt256PasswordHasher',
