8642d7483fd6e7bb9eb66478c8728bcf1849b268
ValiantVaibhav <vaibhav5118402@gmail.com>
2019-02-17 03:24:22 +0530

    Database modification

--- House-renting-system/house_renting_project/settings.py
+++ House-renting-system/house_renting_project/settings.py
@@ -76,68 +76,37 @@ WSGI_APPLICATION = 'house_renting_project.wsgi.application'
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'house_renting_database',
-        'HOST': 'localhost',
-        'USER': 'ibm',
-        'PASSWORD': 'ibm',
+        'ENGINE': 'django.contrib.gis.db.backends.postgresql',
+#        'NAME': 'house_renting_database',
+#        'HOST': 'localhost',
+#        'USER': 'ibm',
+#        'PASSWORD': 'ibm',
     }
-}
+#}
 
 
 ##Password validation
 ## https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 #
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-                         ]
+#AUTH_PASSWORD_VALIDATORS = [
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#    },
+#                         ]
 
 #PASSWORD_HASHERS=[
 #    'django.contrib.auth.hashers.Argon2PasswordHasher',
