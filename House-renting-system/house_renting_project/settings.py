fd148e7675130d9888dc98ed8ecc78175f017f1d
ValiantVaibhav <vaibhav5118402@gmail.com>
2019-02-20 02:07:08 +0530

    adding static path for CSS and Javascript

--- House-renting-system/house_renting_project/settings.py
+++ House-renting-system/house_renting_project/settings.py
@@ -108,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 PASSWORD_HASHERS=[
+    
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCrypt256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
