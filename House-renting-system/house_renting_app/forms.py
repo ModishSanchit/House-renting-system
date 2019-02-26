37fb135a166a7c13b3e9ee5e6adda370e6987eb1
ModishSanchit <ModishSanchit@gmail.com>
2019-02-27 03:41:30 +0530

    Further updation inbthe forms.py file

--- House-renting-system/house_renting_app/forms.py
+++ House-renting-system/house_renting_app/forms.py
@@ -1,6 +1,4 @@
-from django import forms
-from django.contrib.auth.models import User
-from house_renting_app.models import UserProfileInfo
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
