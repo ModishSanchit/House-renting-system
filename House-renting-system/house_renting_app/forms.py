a217edf07eba97082a34d86107538927eb1d36a2
ModishSanchit <ModishSanchit@gmail.com>
2019-02-28 01:44:58 +0530

    Further change in the form section

--- House-renting-system/house_renting_app/forms.py
+++ House-renting-system/house_renting_app/forms.py
@@ -1,11 +1,6 @@
 
 
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
 
-    class Meta:   #This will connect models to the actual form
-        model=User
-        fields=('first_name','last_name','username','password',)
 
 # class UserProfileForm(forms.ModelForm):
 #     class Meta:
