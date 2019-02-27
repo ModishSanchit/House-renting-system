7852cc50d4e658efee860289765f84a8c83397dd
ModishSanchit <ModishSanchit@gmail.com>
2019-02-28 01:51:58 +0530

    Further updation in the form section of the form

--- House-renting-system/house_renting_app/forms.py
+++ House-renting-system/house_renting_app/forms.py
@@ -1,6 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+from house_renting_app.models import UserProfileInfo
 
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
 
-
+    class Meta:   #This will connect models to the actual form
+        model=User
+        fields=('first_name','last_name','username','password',)
 
 # class UserProfileForm(forms.ModelForm):
 #     class Meta:
