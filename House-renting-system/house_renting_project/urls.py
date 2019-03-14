933fc4b7ca7fb34e64332e783cbd7cabbc9a24ad
ValiantVaibhav <vaibhav5118402@gmail.com>
2019-03-14 18:46:25 +0530

    modified url

--- House-renting-system/house_renting_project/urls.py
+++ House-renting-system/house_renting_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from house_renting_app import views
 
 #from django.conf import settings
-#from django.conf.urls.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
